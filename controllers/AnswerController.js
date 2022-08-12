470d1a4bac8332a339e525d113a02a4b163ebe6a
atn95 <antannguyen2000@gmail.com>
2022-08-12 10:56:20 -0500

    fixed some association error

--- controllers/AnswerController.js
+++ controllers/AnswerController.js
@@ -1,11 +1,11 @@
-// const { Answer } = require('../models')
+const { Answer } = require('../models')
 const { getIO } = require('../utils/socket')
 const io = getIO()
 
 const submitAnswer = async (req, res) => {
 	try {
 		//TODO: create answer and link to db
-		// const answer = await Answer.create(req.body)
+		const answer = await Answer.create(req.body)
 		res.status(200).json(req.body)
 	} catch (error) {
 		console.log(error)
