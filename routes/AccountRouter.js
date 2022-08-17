55b6ad2eb3887616af034018404a5f8c52bb5ffe
Ian Hansson <ian713hansson@gmail.com>
2022-08-16 20:43:46 -0500

    trying join-key

--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -1,7 +1,7 @@
 const Router = require('express').Router()
 const controller = require('../controllers/AccountController')
 const middleware = require('../middleware')
-
+Router.put('/client/:client_id/:join_key', controller.joinRoom)
 Router.post('/login', controller.login)
 Router.get(
 	'/session',
@@ -13,5 +13,7 @@ Router.get(
 Router.get('/accounttype/:type/:user_id', controller.getAccountTypeInfoById)
 Router.post('/submit/host', controller.CreateHost)
 Router.post('/submit/client', controller.CreateClient)
+
+
 
 module.exports = Router
