6f448e7f8c45f02982e1a424c13438e2776c5b5f
atn95 <antannguyen2000@gmail.com>
2022-08-19 11:40:30 -0500

    fixing crash

--- routes/RoomRouter.js
+++ routes/RoomRouter.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/RoomController')
 Router.post('/submit', controller.CreateRoom)
 Router.get('/rooms/:owner_id', controller.getRoomByOwnerId)
 Router.get('/rooms/id/:id', controller.getRoomById)
-Router.delete('/delete/:room_id', controller.getQuestionById)
+Router.delete('/delete/:room_id', controller.deleteRoomById)
 
 module.exports = Router
